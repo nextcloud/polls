b53f01a45ae73608d8e566138539fb7b8456cdda
Vinzenz Rosenkranz <vinzenz.rosenkranz@gmail.com>
2016-03-13 23:05:29 +0100

    only use digits and letters as hash

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -31,6 +31,7 @@ use \OCP\IAvatarManager;
 use \OCP\ILogger;
 use \OCP\IRequest;
 use \OCP\IURLGenerator;
+use OCP\Security\ISecureRandom;
 use \OCP\AppFramework\Http\TemplateResponse;
 use \OCP\AppFramework\Http\RedirectResponse;
 use \OCP\AppFramework\Controller;
@@ -264,7 +265,10 @@ class PageController extends Controller {
         $event->setDescription(htmlspecialchars($pollDesc));
         $event->setOwner($userId);
         $event->setCreated(date('Y-m-d H:i:s'));
-        $event->setHash(\OC::$server->getSecureRandom()->getMediumStrengthGenerator()->generate(16));
+        $event->setHash(\OC::$server->getSecureRandom()->getMediumStrengthGenerator()->generate(16,
+			ISecureRandom::CHAR_DIGITS.
+			ISecureRandom::CHAR_LOWER.
+			ISecureRandom::CHAR_UPPER));
 
         $accessValues = json_decode($accessValues);
         $groups = $accessValues->groups;
