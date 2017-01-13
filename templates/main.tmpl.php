8fb0f21b6c9d07a991d7576ce707d3a09ab6d6d4
Arnulf <arnulf.mittelstaedt@gmail.com>
2017-01-13 12:25:32 +0100

    Poll table
    New poll table design.
    
    CSS clean up part 1/?

--- templates/main.tmpl.php
+++ templates/main.tmpl.php
@@ -2,10 +2,9 @@
     \OCP\Util::addStyle('polls', 'main');
     \OCP\Util::addStyle('polls', 'list');
     \OCP\Util::addStyle('polls', 'sidebar');
-    //\OCP\Util::addStyle('polls', 'bootstrap.min');
     \OCP\Util::addScript('polls', 'start');
-    \OCP\Util::addScript('polls', 'bootstrap.min');
-    \OCP\Util::addScript('polls', 'menu');
+    //\OCP\Util::addScript('polls', 'bootstrap.min');
+    //\OCP\Util::addScript('polls', 'menu');
     use OCP\User;
     $userId = $_['userId'];
     $userMgr = $_['userMgr'];
@@ -36,15 +35,16 @@
     <table id="polltable" class="has-controls">
       <thead>
         <tr>
-          <th id="headerName" class="column-name">
-            <div id="headerName-container">
-					    <a class="name sort columntitle" data-sort="name"><span><?php p($l->t('Title')); ?></span><span class="sort-indicator icon-triangle-n"></span></a>
-				    </div>
+          <th>
+            <a class="sort columntitle" data-sort="name"><span><?php p($l->t('Title')); ?></span><span class="sort-indicator icon-triangle-n"></span></a>
           </th>
           <th><?php p($l->t('Description')); ?></th>
           <th><?php p($l->t('Created')); ?></th>
           <th><?php p($l->t('By')); ?></th>
           <th><?php p($l->t('Expires')); ?></th>
+          <th><?php p($l->t('participated')); ?></th>
+            <th id="id_th_descr"><?php p($l->t('Access')); ?></th>
+            <th><?php p($l->t('Options')); ?></th>
         </tr>
       </thead>
       <tbody>
@@ -58,7 +58,7 @@
             <tr>
                 <td class="pollname">
                   <div class="thumbnail" style="background-image:url(/apps/polls/img/poll-icon.svg); background-size: 32px;"></div>
-                    <a class="name" href="<?php p($pollUrl); ?>"><span class="nametext"><span class="innernametext"><?php p($poll->getTitle()); ?></span></span></a>
+                    <a class="name" href="<?php p($pollUrl); ?>"><?php p($poll->getTitle()); ?></a>
                 </td>
                 <?php
                     $desc_str = $poll->getDescription();
@@ -67,7 +67,7 @@
                         $desc_str = substr($desc_str, 0, 80) . '...';
                     }
                 ?>
-                <td class="pollname"><span class="name"><?php p($desc_str); ?></span></td>
+                <td ><?php p($desc_str); ?></td>
                 <td><?php p(date('d.m.Y H:i', strtotime($poll->getCreated()))); ?></td>
                 <td>
                     <?php
@@ -87,7 +87,45 @@
                             print_unescaped('<td>' . $l->t('Never') . '</td>');
                         }
                     ?>
-
+                    <td>
+                                        <?php
+                                            $partic_class = 'partic_no';
+                                            $partic_polls = $_['participations'];
+                                            for($i = 0; $i < count($partic_polls); $i++){
+                                                if($poll->getId() == intval($partic_polls[$i]->getPollId())){
+                                                    $partic_class = 'partic_yes';
+                                                    array_splice($partic_polls, $i, 1);
+                                                    break;
+                                                }
+                                            }
+                                        ?>
+                                        <div class="partic_all <?php p($partic_class); ?>">
+                                        </div>
+                                        |
+                                        <?php
+                                            $partic_class = 'partic_no';
+                                            $partic_comm = $_['comments'];
+                                            for($i = 0; $i < count($partic_comm); $i++){
+                                                if($poll->getId() === intval($partic_comm[$i]->getPollId())){
+                                                    $partic_class = 'partic_yes';
+                                                    array_splice($partic_comm, $i, 1);
+                                                    break;
+                                                }
+                                            }
+                                        ?>
+                                        <div class="partic_all <?php p($partic_class); ?>">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <?php p($l->t($poll->getAccess())); ?>
+                                    </td>
+                                    <td>
+                                        <?php if ($poll->getOwner() === $userId) : ?>
+                                        <input type="button" id="id_del_<?php p($poll->getId()); ?>" class="table_button cl_delete icon-delete"></input>
+                                        <a href="<?php p($urlGenerator->linkToRoute('polls.page.edit_poll', ['hash' => $poll->getHash()])); ?>"><input type="button" id="id_edit_<?php p($poll->getId()); ?>" class="table_button cl_edit icon-rename"></input></a>
+                                        <?php endif; ?>
+                                        <input type="button" class="table_button cl_link icon-public" data-url="<?php p(OCP\Util::linkToAbsolute('', $pollUrl)); ?>" title="<?php p($l->t('Click to get link')); ?>"></input>
+                                    </td>
             </tr>
         <?php endforeach; ?>
         </tbody>
