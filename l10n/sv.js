daa4e2c5d41030ca0540a220b7972f8986ceb25f
dartcafe <github@dartcafe.de>
2017-10-28 08:23:13 +0200

    manually sync l10n with master

---
+++ l10n/sv.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "polls",
+    {
+    "Title" : "Titel",
+    "Description" : "Beskrivning",
+    "Access" : "Åtkomst",
+    "Registered users only" : "Endast registrerade användare",
+    "hidden" : "gömd",
+    "Public access" : "Öppen adress",
+    "Select" : "Väl",
+    "User/Group search" : "Användar/grupp-sökning",
+    "Anonymous" : "Anonym",
+    "Hide user names for admin" : "Göm användarnamn för administratör",
+    "Expires" : "Går ut",
+    "Text based" : "Textbaserad",
+    "Dates" : "Datum",
+    "Add" : "Lägg till",
+    "Update poll" : "Uppdatera omröstning"
+},
+"nplurals=2; plural=(n != 1);");
