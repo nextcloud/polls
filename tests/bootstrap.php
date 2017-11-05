c9587d7cb0bb4781085294d5ed7246b3ae8588c2
splitt3r <splitt3r@users.noreply.github.com>
2017-11-05 18:12:10 +0100

    Small doc block update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,14 +25,14 @@ if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
-require_once(__DIR__.'/../../../lib/base.php');
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 \OC_App::loadApp('polls');
 
 if (!class_exists('PHPUnit_Framework_TestCase')) {
-	require_once('PHPUnit/Autoload.php');
+	require_once 'PHPUnit/Autoload.php';
 }
 
-OC_Hook::clear();
+\OC_Hook::clear();
