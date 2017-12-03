1892cbe20b44728530f436ec9525164c3ce2f050
splitt3r <splitt3r@users.noreply.github.com>
2017-12-03 19:14:36 +0100

    Added license header to migration + type hint update

--- lib/Migration/Version009000Date20171202105141.php
+++ lib/Migration/Version009000Date20171202105141.php
@@ -1,25 +1,47 @@
 <?php
+/**
+ * @copyright Copyright (c) 2017 René Gieling <github@dartcafe.de>
+ *
+ * @author René Gieling <github@dartcafe.de>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 namespace OCA\Polls\Migration;
 
-use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Type;
+use OC\DB\SchemaWrapper;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
 
 /**
- * Auto-generated migration step: Please modify to your needs!
+ * Installation class for the polls app.
  */
 class Version009000Date20171202105141 extends SimpleMigrationStep {
 
 	/**
 	 * @param IOutput $output
-	 * @param \Closure $schemaClosure The `\Closure` returns a `Schema`
+	 * @param \Closure $schemaClosure The `\Closure` returns a `SchemaWrapper`
 	 * @param array $options
-	 * @return null|Schema
+	 * @return null|SchemaWrapper
 	 * @since 13.0.0
 	 */
 	public function changeSchema(IOutput $output, \Closure $schemaClosure, array $options) {
-		/** @var Schema $schema */
+		/** @var SchemaWrapper $schema */
 		$schema = $schemaClosure();
 
 		if (!$schema->hasTable('polls_events')) {
