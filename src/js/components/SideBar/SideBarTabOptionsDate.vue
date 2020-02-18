f0414f118b2f706a5ef5531620e108224e690d69
dartcafe <github@dartcafe.de>
2020-02-18 08:34:58 +0100

    fix #809

--- src/js/components/SideBar/SideBarTabOptionsDate.vue
+++ src/js/components/SideBar/SideBarTabOptionsDate.vue
@@ -40,7 +40,7 @@
 			<div>
 				<div class="selectUnit">
 					<input v-model="move.step">
-					<Multiselect v-model="move.unit" :options="move.units" />
+					<Multiselect v-model="move.unit" :options="move.units" label="name" track-by="name" />
 				</div>
 			</div>
 			<div>
@@ -91,7 +91,14 @@ export default {
 			move: {
 				step: 1,
 				unit: 'week',
-				units: ['minute', 'hour', 'day', 'week', 'month', 'year']
+				units: [
+					{ name: t('polls', 'Minute'), value: 'minute' },
+					{ name: t('polls', 'Hour'), value: 'hour' },
+					{ name: t('polls', 'Day'), value: 'day' },
+					{ name: t('polls', 'Week'), value: 'week' },
+					{ name: t('polls', 'Month'), value: 'month' },
+					{ name: t('polls', 'Year'), value: 'year' }
+				]
 			}
 		}
 	},
@@ -154,7 +161,7 @@ export default {
 			const store = this.$store
 			this.options.list.forEach(function(existingOption) {
 				const option = Object.assign({}, existingOption)
-				option.pollOptionText = moment(option.pollOptionText).add(payload.step, payload.unit).format('YYYY-MM-DD HH:mm:ss')
+				option.pollOptionText = moment(option.pollOptionText).add(payload.step, payload.unit.value).format('YYYY-MM-DD HH:mm:ss')
 				option.timestamp = moment.utc(option.pollOptionText).unix()
 				store.dispatch('updateOptionAsync', { option: option })
 			})
