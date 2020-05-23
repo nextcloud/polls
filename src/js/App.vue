8a5920bfbefa4085e5d14977d288636762c46348
dartcafe <github@dartcafe.de>
2020-05-23 11:15:04 +0200

    experimental background style

--- src/js/App.vue
+++ src/js/App.vue
@@ -301,6 +301,44 @@ input {
 	min-width: 320px;
 }
 
+// experimental colored background in the main area
+.app-polls.bgcolored {
+	.app-navigation {
+		border-right: 0px;
+		box-shadow: 2px 0 6px var(--color-box-shadow);
+	}
+	.app-content {
+		background-color: var(--color-primary-light);
+		[class*='area__'] {
+			box-shadow: 2px 2px 6px var(--color-box-shadow);
+			margin: 12px;
+		}
+	}
+}
+
+// experimental background image
+.app-polls.bgimage {
+	background: url('https://images.unsplash.com/photo-1589967698280-1e86b3d8c1ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80)');
+	background-size: cover;
+	.app-navigation {
+		backdrop-filter: blur(10px);
+		background-color: rgba(255, 255, 255, 0.5);
+		border-right: 0px;
+		box-shadow: 2px 0 6px var(--color-box-shadow);
+	}
+	.app-content {
+		background-color: transparent;
+	}
+	.app-sidebar {
+		backdrop-filter: blur(10px);
+		background-color: rgba(255, 255, 255, 0.5);
+	}
+	[class*='area__'] {
+		box-shadow: 2px 2px 6px var(--color-box-shadow);
+		margin: 12px;
+	}
+}
+
 [class*='area__'] {
 	padding: 8px;
 	background-color: var(--color-main-background);
