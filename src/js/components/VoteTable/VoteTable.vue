6b2496cb4838e3e05fb8cb0862604036df5c8ab8
dartcafe <github@dartcafe.de>
2020-08-23 08:03:02 +0200

    fix linter hints

--- src/js/components/VoteTable/VoteTable.vue
+++ src/js/components/VoteTable/VoteTable.vue
@@ -150,7 +150,7 @@ export default {
 				userId: this.userToRemove,
 			})
 				.then(() => {
-					showSuccess(t('polls', 'User {userId} removed', { userId: this.userToRemove} ))
+					showSuccess(t('polls', 'User {userId} removed', { userId: this.userToRemove }))
 				})
 			this.modal = false
 			this.userToRemove = ''
