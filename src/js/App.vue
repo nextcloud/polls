2055e7a9f9e0c9c0a3c84840fe88c012d70204ab
dartcafe <github@dartcafe.de>
2021-01-05 11:11:21 +0100

    fix route watch
    Signed-off-by: dartcafe <github@dartcafe.de>

--- src/js/App.vue
+++ src/js/App.vue
@@ -84,7 +84,7 @@ export default {
 	},
 
 	watch: {
-		$route(from, to) {
+		$route(to, from) {
 			console.log('route changed', 'from ' + from.params.id + ' to ' + to.params.id)
 			if (from.params.id !== to.params.id) {
 				console.log('poll id', this.$route.params.id)
