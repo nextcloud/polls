6da99a94cb2edf101594ae37a9412aeaf1f70a6e
dartcafe <github@dartcafe.de>
2021-01-20 22:42:34 +0100

    add warning class

--- src/js/components/SideBar/SideBarTabConfiguration.vue
+++ src/js/components/SideBar/SideBarTabConfiguration.vue
@@ -22,7 +22,7 @@
 
 <template>
 	<div>
-		<div v-if="participantsVoted">
+		<div v-if="participantsVoted" class="warning">
 			{{ t('polls', 'Please be careful when changing options, because it can affect existing votes in an unwanted manner.') }}
 		</div>
 
@@ -42,8 +42,8 @@
 				type="checkbox"
 				class="checkbox">
 			<label for="allowMaybe"> {{ t('polls', 'Allow "maybe" vote') }}</label>
-			<div v-if="(useVoteLimit || useOptionLimit) && pollAllowMaybe" class="indented">
-				{{ t('polls', 'If vote limits are used, "maybe" should be not be allowed.') }}
+			<div v-if="(useVoteLimit || useOptionLimit) && pollAllowMaybe" class="indented warning">
+				{{ t('polls', 'If vote limits are used, "maybe" shouldn\'t be allowed.') }}
 			</div>
 			<input id="anonymous"
 				v-model="pollAnonymous"
