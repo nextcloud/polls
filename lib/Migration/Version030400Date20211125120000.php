de78a102c4fe23dad36a34e5802b0ab08fc2d329
dartcafe <github@dartcafe.de>
2021-11-25 21:09:12 +0100

    update migration
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Migration/Version030400Date20211125120000.php
+++ lib/Migration/Version030400Date20211125120000.php
@@ -38,7 +38,7 @@ use OCA\Polls\Db\Poll;
  * Changed class naming: Version[jjmmpp]Date[YYYYMMDDHHMMSS]
  * Version: jj = major version, mm = minor, pp = patch
  */
-class Version030200Date20210912120000 extends SimpleMigrationStep {
+class Version030400Date20211125120000 extends SimpleMigrationStep {
 
 	/** @var IDBConnection */
 	protected $connection;
