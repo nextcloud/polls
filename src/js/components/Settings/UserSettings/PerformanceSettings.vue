4fd62c4385b77b28b5537d6d1d927ee11da87b13
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-02-25 10:29:38 +0100

    l10n: Correct a word
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- src/js/components/Settings/UserSettings/PerformanceSettings.vue
+++ src/js/components/Settings/UserSettings/PerformanceSettings.vue
@@ -24,7 +24,7 @@
 	<div class="user_settings">
 		<span>
 			{{ t('polls', 'A poll with many options and voters can have a heavy impact on client performance.') }}
-			{{ t('polls', 'Set the amount of vote cells (options x participants) up to which all vote cells should be displayed.') }}
+			{{ t('polls', 'Set the amount of voting cells (options x participants) up to which all voting cells should be displayed.') }}
 			{{ t('polls', 'If this threshold gets tresspasses only the current user will be displayed, to avoid a performance breakdown.') }}
 			{{ t('polls', 'The default threshold of 1.000 should be a good and safe value.') }}
 		</span>
