4c32d5c05cd05dd6003e95aba0d135c28dac7dbe
dartcafe <github@dartcafe.de>
2024-02-10 13:43:13 +0100

    removed obsolete groupBy
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Db/PollMapper.php
+++ lib/Db/PollMapper.php
@@ -170,8 +170,7 @@ class PollMapper extends QBMapper {
 		$qb = $this->db->getQueryBuilder();
 
 		$qb->select(self::TABLE . '.*')
-			->from($this->getTableName(), self::TABLE)
-			->groupBy(self::TABLE . '.id');
+			->from($this->getTableName(), self::TABLE);
 
 		return $qb;
 	}
