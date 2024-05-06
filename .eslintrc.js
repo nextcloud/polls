58768b1a0d535edfe0677a0c0372a3316d1255da
René Gieling <github@dartcafe.de>
2024-05-06 22:38:06 +0200

    change config to support prettier (#3482)
    Signed-off-by: dartcafe <github@dartcafe.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
 	extends: [
-		'plugin:promise/recommended',
-		'plugin:vue/essential',
 		'@nextcloud',
+		'prettier',
 	],
 	plugins: [
 		'promise',
 	],
 	rules: {
-		'arrow-parens': 'error',
 		'arrow-body-style': 'error',
-		'brace-style': 'error',
-		'newline-per-chained-call': ['error', { ignoreChainWithDepth: 4 }],
 		'no-array-constructor': 'error',
 		'no-continue': 'error',
 		'no-else-return': ['error', { allowElseIf: false }],
 		'no-lonely-if': 'error',
 		'no-negated-condition': 'error',
 		'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-		'object-curly-spacing': ['error', 'always'],
 		'prefer-template': 'error',
 		'vue/no-unused-properties': ['error', {
 			groups: ['props', 'data', 'computed', 'methods'],
