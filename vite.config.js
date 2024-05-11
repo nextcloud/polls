af478f79575bffeb0dd5238cdb29c8ac4182540a
dartcafe <github@dartcafe.de>
2024-05-11 21:01:00 +0200

    heal config
    Signed-off-by: dartcafe <github@dartcafe.de>

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
 import { createAppConfig } from '@nextcloud/vite-config'
 import { join, resolve } from 'path'
 
-export default createAppConfig(
-	{
-		main: resolve(join('src/js', 'main.js')),
-		userSettings: resolve(join('src/js', 'userSettings.js')),
-		adminSettings: resolve(join('src/js', 'adminSettings.js')),
-		dashboard: resolve(join('src/js', 'dashboard.js')),
-	},
-	{
-		config: {
-			build: {
-				cssCodeSplit: true,
-				rollupOptions: {
-					output: {
-						manualChunks: {
-							vendor: ['vue', 'vue-router'],
-						},
-					},
-				},
-			},
-		},
-		inlineCSS: false,
-	},
-)
+export default createAppConfig({
+	main: resolve(join('src/js', 'main.js')),
+	userSettings: resolve(join('src/js', 'userSettings.js')),
+	adminSettings: resolve(join('src/js', 'adminSettings.js')),
+	dashboard: resolve(join('src/js', 'dashboard.js')),
+}, {
+	inlineCSS: { relativeCSSInjection: true },
+})
