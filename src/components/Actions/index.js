8c0d0cceb24aa19ebc831e2d68cc81e48a5a2883
dartcafe <github@dartcafe.de>
2024-12-25 12:25:38 +0100

    move sorting and view mode to user's menu
    Signed-off-by: dartcafe <github@dartcafe.de>

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -2,13 +2,11 @@
  * SPDX-FileCopyrightText: 2023 Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-export { default as ActionChangeView } from './modules/ActionChangeView.vue'
 export { default as ActionDelete } from './modules/ActionDelete.vue'
 export { default as ActionDeleteOrphanedVotes } from './modules/ActionDeleteOrphanedVotes.vue'
 export { default as ActionOpenOptionsSidebar } from './modules/ActionOpenOptionsSidebar.vue'
 export { default as ActionOpenSharesSidebar } from './modules/ActionOpenSharesSidebar.vue'
 export { default as ActionRegister } from './modules/ActionRegister.vue'
 export { default as ActionSendConfirmed } from './modules/ActionSendConfirmed.vue'
-export { default as ActionSortOptions } from './modules/ActionSortOptions.vue'
 export { default as ActionSwitchSafeTable } from './modules/ActionSwitchSafeTable.vue'
 export { default as ActionToggleSidebar } from './modules/ActionToggleSidebar.vue'
