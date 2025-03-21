d4b3e77fecd9c1805a44ffe830e00ba2c538f502
dartcafe <github@dartcafe.de>
2025-03-21 23:24:34 +0100

    fix php-cs-fixer
    Signed-off-by: dartcafe <github@dartcafe.de>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2021 Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-require_once 'vendor/autoload.php';
+require_once './vendor-bin/php-cs-fixer/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
-use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $config = new Config();
 $config
-	->setParallelConfig(ParallelConfigFactory::detect())
 	->getFinder()
 	->ignoreVCSIgnored(true)
 	->notPath('build')
