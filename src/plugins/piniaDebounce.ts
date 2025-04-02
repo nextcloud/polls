3901276f8bb5847e578627992f119788e80684c1
dartcafe <github@dartcafe.de>
2025-04-02 20:27:01 +0200

    REUSE fix
    Signed-off-by: dartcafe <github@dartcafe.de>

--- src/plugins/piniaDebounce.ts
+++ src/plugins/piniaDebounce.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import { PiniaPluginContext } from 'pinia'
 import debounce from 'lodash/debounce'
 
