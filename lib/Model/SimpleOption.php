232a464fe5594f162daa11e0ac7bf8bfda3857d5
dartcafe <github@dartcafe.de>
2025-04-10 21:56:57 +0200

    fix SimpleOption on null values
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Model/SimpleOption.php
+++ lib/Model/SimpleOption.php
@@ -15,21 +15,22 @@ class SimpleOption implements JsonSerializable {
 	 * @param string $text The text of the option
 	 * @param int $timestamp The timestamp for the datepoll
 	 * @param int $duration The duration of the option
+	 * @param int $order The order of the option
 	 */
 	public function __construct(
 		protected string $text,
-		protected int $timestamp,
-		protected int $duration = 0,
-		protected int $order = 0,
+		protected ?int $timestamp,
+		protected ?int $duration = 0,
+		protected ?int $order = 0,
 	) {
 	}
 
 	public function jsonSerialize(): array {
 		return [
 			'text' => $this->text,
-			'timestamp' => $this->timestamp,
-			'duration' => $this->duration,
-			'order' => $this->order,
+			'timestamp' => $this->timestamp ?? 0,
+			'duration' => $this->duration ?? 0,
+			'order' => $this->order ?? 0,
 		];
 	}
 
@@ -38,15 +39,15 @@ class SimpleOption implements JsonSerializable {
 	}
 
 	public function getTimestamp(): int {
-		return $this->timestamp;
+		return $this->timestamp ?? 0;
 	}
 
 	public function getDuration(): int {
-		return $this->duration;
+		return $this->duration ?? 0;
 	}
 
 	public function getOrder(): int {
-		return $this->order;
+		return $this->order ?? 0;
 	}
 
 	public function setOrder(int $order): void {
@@ -56,8 +57,8 @@ class SimpleOption implements JsonSerializable {
 	public static function fromArray(array $option): SimpleOption {
 		return new SimpleOption(
 			$option['text'],
-			$option['timestamp'],
-			$option['duration'],
+			$option['timestamp'] ?? 0,
+			$option['duration'] ?? 0,
 			$option['order'] ?? 0,
 		);
 	}
