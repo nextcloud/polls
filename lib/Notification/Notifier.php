593d5913ca5389318f6768d884db9835a37d0716
dartcafe <github@dartcafe.de>
2025-05-14 19:30:51 +0200

    fix notifications
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Notification/Notifier.php
+++ lib/Notification/Notifier.php
@@ -17,6 +17,7 @@ use OCP\IURLGenerator;
 use OCP\L10N\IFactory;
 use OCP\Notification\INotification;
 use OCP\Notification\INotifier;
+use Psr\Log\LoggerInterface;
 
 class Notifier implements INotifier {
 	public const NOTIFY_POLL_DELETED_BY_OTHER = 'deletePollByOther';
@@ -34,6 +35,7 @@ class Notifier implements INotifier {
 		protected PollMapper $pollMapper,
 		private UserMapper $userMapper,
 		private NotificationService $notificationService,
+		private LoggerInterface $logger,
 	) {
 	}
 
@@ -51,22 +53,6 @@ class Notifier implements INotifier {
 		return $this->l10nFactory->get(AppConstants::APP_ID)->t('Polls');
 	}
 
-	/**
-	 * @return string[][]
-	 *
-	 * @psalm-return array{actor: array{type: 'user', id: string, name: string}}
-	 */
-	private function getActor(string $actorId): array {
-		$actor = $this->userMapper->getUserFromUserBase($actorId);
-		return [
-			'actor' => [
-				'type' => 'user',
-				'id' => $actor->getId(),
-				'name' => $actor->getDisplayName(),
-			]
-		];
-	}
-
 	public function prepare(INotification $notification, string $languageCode): INotification {
 		$l = $this->l10nFactory->get(AppConstants::APP_ID, $languageCode);
 		if ($notification->getApp() !== AppConstants::APP_ID) {
@@ -82,41 +68,71 @@ class Notifier implements INotifier {
 
 		try {
 			$poll = $this->pollMapper->get(intval($notification->getObjectId()));
-			$actor = $this->getActor($parameters['actor'] ?? $poll->getOwner());
-			$pollTitle = $parameters['pollTitle'] ?? $poll->getTitle();
-			$notification->setLink($poll->getVoteUrl());
 		} catch (DoesNotExistException $e) {
+			$this->logger->info('Notification silently removed, poll not found', [
+				'notification' => $notification->getObjectId(),
+				'error' => $e->getMessage(),
+			]);
 			$this->notificationService->removeNotification(intval($notification->getObjectId()));
 			return $notification;
 		}
 
+		$actor = $this->userMapper->getUserFromUserBase($parameters['actor'] ?? $poll->getOwner());
+		$pollTitle = $parameters['pollTitle'] ?? $poll->getTitle();
+		$notification->setLink($poll->getVoteUrl());
+
 		$subjects = match ($notification->getSubject()) {
 			self::NOTIFY_INVITATION => [
-				self::SUBJECT_PARSED => $l->t('%s invited you to a poll', $actor['actor']['name']),
+				self::SUBJECT_PARSED => $l->t('%s invited you to a poll', $actor->getDisplayName()),
 				self::SUBJECT_RICH => $l->t('{actor} has invited you to the poll "%s".', $pollTitle),
 			],
 			self::NOTIFY_POLL_TAKEOVER => [
-				self::SUBJECT_PARSED => $l->t('%s took over your poll', $actor['actor']['name']),
+				self::SUBJECT_PARSED => $l->t('%s took over your poll', $actor->getDisplayName()),
 				self::SUBJECT_RICH => $l->t('{actor} took over your poll "%s" and is the new owner.', $pollTitle),
 			],
 			self::NOTIFY_POLL_CHANGED_OWNER => [
 				self::SUBJECT_PARSED => $l->t('%s is the new owner of your poll. ', $parameters['newOwner']),
-				self::SUBJECT_RICH => $l->t('{actor} transfered your poll "%2$s" to the new owner %1$s. You are no more the owner.', $parameters['newOwner'], $pollTitle),
+				self::SUBJECT_RICH => $l->t('{actor} transfered your poll "%s" to the new owner {newOwner}. You are no more the owner.', $pollTitle),
 			],
 			self::NOTIFY_POLL_DELETED_BY_OTHER => [
-				self::SUBJECT_PARSED => $l->t('%s deleted your poll', $actor['actor']['name']),
+				self::SUBJECT_PARSED => $l->t('%s deleted your poll', $actor->getDisplayName()),
 				self::SUBJECT_RICH => $l->t('{actor} deleted your poll "%s".', $pollTitle),
 			],
 			self::NOTIFY_POLL_ARCHIVED_BY_OTHER => [
-				self::SUBJECT_PARSED => $l->t('%s archived your poll', $actor['actor']['name']),
+				self::SUBJECT_PARSED => $l->t('%s archived your poll', $actor->getDisplayName()),
 				self::SUBJECT_RICH => $l->t('{actor} archived your poll "%s".', $pollTitle),
 			],
 			// Unknown subject => Unknown notification => throw
 			default => throw new \InvalidArgumentException(),
 		};
 
-		$notification->setParsedSubject($subjects[self::SUBJECT_PARSED]);
-		$notification->setRichSubject($subjects[self::SUBJECT_RICH], $actor);
+		switch ($notification->getSubject()) {
+			case self::NOTIFY_POLL_CHANGED_OWNER:
+				$newOwner = $this->userMapper->getUserFromUserBase($parameters['newOwner']);
+				// overwrite the subject with the new owner
+				$notification->setParsedSubject(
+					$l->t('%s is the new owner of your poll. ', $newOwner->getDisplayName())
+				);
+
+				$notification->setRichSubject(
+					$subjects[self::SUBJECT_RICH],
+					[
+						'actor' => $actor->getRichObjectString(),
+						'newOwner' => $newOwner->getRichObjectString(),
+					]
+				);
+				break;
+
+			default:
+				$notification->setParsedSubject($subjects[self::SUBJECT_PARSED]);
+				$notification->setRichSubject(
+					$subjects[self::SUBJECT_RICH],
+					[
+						'actor' => $actor->getRichObjectString(),
+					]
+				);
+				break;
+		}
 
 		return $notification;
 	}
