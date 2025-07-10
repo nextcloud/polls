39c9b2604dc493ffe85624336b263559bffdf506
dartcafe <github@dartcafe.de>
2025-07-10 10:14:28 +0200

    fix optionsCount
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Db/PollMapper.php
+++ lib/Db/PollMapper.php
@@ -362,7 +362,7 @@ class PollMapper extends QBMapper {
 		$qb->addSelect($qb->createFunction('COUNT(DISTINCT(CASE WHEN ' . $joinAlias . '.owner != \'\' THEN 1 END)) AS proposals_count'));
 
 		// count number of options by counting unique ids
-		$qb->selectAlias($qb->func()->count($joinAlias . '.id'), 'optionsCount');
+		$qb->selectAlias($qb->createFunction('COUNT(DISTINCT(' . $joinAlias . '.id))'), 'optionsCount');
 
 		$qb->leftJoin(
 			$fromAlias,
