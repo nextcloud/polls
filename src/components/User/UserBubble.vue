38f4b7c0eb08d8ac863a2f07f6b580bdfaad4318
dartcafe <github@dartcafe.de>
2025-07-18 12:47:26 +0200

    wrap NcUserBubble into UserBubble
    Signed-off-by: dartcafe <github@dartcafe.de>

---
+++ src/components/User/UserBubble.vue
@@ -0,0 +1,48 @@
+<!--
+  - SPDX-FileCopyrightText: 2018 Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { User } from '../../Types/index.ts'
+import { NcUserBubble } from '@nextcloud/vue'
+import { t } from '@nextcloud/l10n'
+
+interface Props {
+	user: User
+}
+
+const {
+	user = {
+		id: '',
+		displayName: '',
+		emailAddress: '',
+		isNoUser: true,
+		isAdmin: false,
+		isGuest: false,
+		type: '',
+		subName: null,
+		subtitle: null,
+		desc: null,
+		organisation: null,
+		languageCode: '',
+		localeCode: null,
+		timeZone: null,
+		categories: null,
+	},
+} = defineProps<Props>()
+
+const bubbleProps = computed(() => ({
+	user: user.isNoUser || user.isGuest ? undefined : user.id,
+	displayName:
+		user.type === 'deleted'
+			? t('polls', 'Deleted participant')
+			: user.displayName,
+}))
+</script>
+
+<template>
+	<NcUserBubble v-bind="bubbleProps" />
+</template>
