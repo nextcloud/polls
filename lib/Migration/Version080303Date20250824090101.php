99e4d677f16099714ecf45f23695cd2c536fa76a
dartcafe <github@dartcafe.de>
2025-08-24 09:01:49 +0200

    force migration run
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Migration/Version080303Date20250824090101.php
+++ lib/Migration/Version080303Date20250824090101.php
@@ -24,7 +24,7 @@ use OCP\Migration\SimpleMigrationStep;
  *
  * @psalm-suppress UnusedClass
  */
-class Version080301Date20250822182903 extends SimpleMigrationStep {
+class Version080303Date20250824090101 extends SimpleMigrationStep {
 	private ISchemaWrapper $schema;
 	private ?IOutput $output = null;
 
