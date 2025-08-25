b1fa71d42a3032234b868d872f783bee6e75aaff
dartcafe <github@dartcafe.de>
2025-08-25 08:32:36 +0200

    fix namespace
    Signed-off-by: dartcafe <github@dartcafe.de>

--- lib/Db/V3/IndexManager.php
+++ lib/Db/V3/IndexManager.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\Polls\Db\V2;
+namespace OCA\Polls\Db\V3;
 
 use Doctrine\DBAL\Schema\Exception\IndexDoesNotExist;
 use Doctrine\DBAL\Schema\Schema;
