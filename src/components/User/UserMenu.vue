79ec48b24a82e95c9080aead0efc97806d95158c
dartcafe <github@dartcafe.de>
2026-01-18 20:53:01 +0100

    bring back missing user menu items in table view
    Signed-off-by: dartcafe <github@dartcafe.de>

--- src/components/User/UserMenu.vue
+++ src/components/User/UserMenu.vue
@@ -407,7 +407,6 @@ async function submitEmail() {
 		</NcActionButton>
 
 		<NcActionCheckbox
-			v-if="pollStore.viewMode === 'list-view'"
 			:model-value="subscriptionStore.subscribed"
 			:disabled="!pollStore.permissions.subscribe"
 			title="check"
@@ -439,7 +438,7 @@ async function submitEmail() {
 		</NcActionButton>
 
 		<NcActionButton
-			v-if="pollStore.permissions.vote && pollStore.viewMode === 'list-view'"
+			v-if="pollStore.permissions.vote"
 			:name="t('polls', 'Reset your votes')"
 			:aria-label="t('polls', 'Reset your votes')"
 			@click="resetVotes()">
